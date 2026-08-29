18842b9824342ef9231b5b51b85938e11b61758a
Jogy Felix <jogyfelix1@gmail.com>
2026-08-30 01:39:06 +0530

    chore: github pages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://Ashy2219.github.io',
+	base: '/ashy-portfolio/',
+});
